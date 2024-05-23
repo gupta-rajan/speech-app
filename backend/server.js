a803400013b1cb19b33ed7af95262a0f89d413d5
gupta-rajan <rjngupta22@gmail.com>
2024-05-23 14:22:16 +0530

    changes for images

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
-// import { fileURLToPath } from 'url';
 
 dotenv.config();
 import connectDB from './config/db.js';
@@ -81,21 +80,16 @@ app.use('/api/users', userRoutes);
 app.use('/api/uploads', uploadRoutes);
 app.use('/api/event-uploads', uploadEventRoutes);
 
-// Define __dirname for ES modules
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use('/uploads', express.static('/var/data/uploads'));
     app.use(express.static(path.join(__dirname, '/frontend/build')));
   
     app.get('*', (req, res) =>
       res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     );
 } else {
-    const __dirname = path.resolve();
-    app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
     app.get('/', (req, res) => {
       res.send('API is running....');
     });
