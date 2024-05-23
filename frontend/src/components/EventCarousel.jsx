268ef2556b9fdf8503709306bcc88c1ad5cae02b
gupta-rajan <rjngupta22@gmail.com>
2024-05-23 19:17:14 +0530

    update eventCarousel

--- frontend/src/components/EventCarousel.jsx
+++ frontend/src/components/EventCarousel.jsx
@@ -1,11 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Carousel, Image } from 'react-bootstrap';
+import { Carousel, Image, Spinner } from 'react-bootstrap';
 import Loader from './Loader';
 import Message from './Message';
 import { useGetEventsQuery } from '../slices/eventsApiSlice';
 
 const EventCarousel = () => {
     const { data: events, isLoading, error } = useGetEventsQuery();
+    const [imagesLoaded, setImagesLoaded] = useState({});
+
+    useEffect(() => {
+        if (events) {
+            const initialLoadState = {};
+            events.forEach(event => {
+                initialLoadState[event._id] = event.images.map(() => false);
+            });
+            setImagesLoaded(initialLoadState);
+        }
+    }, [events]);
+
+    const handleImageLoad = (eventId, imageIndex) => {
+        setImagesLoaded(prevState => {
+            const updatedLoadState = { ...prevState };
+            updatedLoadState[eventId][imageIndex] = true;
+            return updatedLoadState;
+        });
+    };
 
     return isLoading ? (
         <Loader />
@@ -13,7 +33,7 @@ const EventCarousel = () => {
         <Message variant='danger'>{error}</Message>
     ) : (
         <>
-            {events.map((event) => (
+            {events.map(event => (
                 event.images && event.images.length > 0 && (
                     <div key={event._id} className='mb-4'>
                         <h2 className='text-center'>{event.name}</h2>
@@ -22,7 +42,23 @@ const EventCarousel = () => {
                                 <Carousel.Item key={index}>
                                     <Link to={event.website}>
                                         <div className='carousel-image-wrapper'>
-                                            <Image src={image} alt={`${event.name} Image ${index}`} className='carousel-image' />
+                                            {!imagesLoaded[event._id] || !imagesLoaded[event._id][index] ? (
+                                                <div className="image-placeholder">
+                                                    <Spinner animation="border" variant="primary" />
+                                                    <Image
+                                                        src={image}
+                                                        alt={`${event.name} Image ${index}`}
+                                                        onLoad={() => handleImageLoad(event._id, index)}
+                                                        style={{ display: 'none' }}
+                                                    />
+                                                </div>
+                                            ) : (
+                                                <Image
+                                                    src={image}
+                                                    alt={`${event.name} Image ${index}`}
+                                                    className='carousel-image'
+                                                />
+                                            )}
                                         </div>
                                         <Carousel.Caption className='carousel-caption'>
                                             <h2 className='text-white text-right'>
