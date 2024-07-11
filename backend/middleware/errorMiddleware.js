254ffcb8e3fd2bd523b81e3d71c78586efba0766
gupta-rajan <rjngupta22@gmail.com>
2024-07-11 18:10:42 +0530

    update error middleware

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,23 +1,20 @@
-const notFound = (req,res,next)=>{
-    const error= new Error(`Not Found - ${req.originalUrl}`);
+const notFound = (req, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
-};
-
-const errorHandler = (err,req,res,next)=>{
-    let statusCode = res.statusCode === 200?500: res.statusCode;
+  };
+  
+  const errorHandler = (err, req, res, next) => {
+    let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     let message = err.message;
-
-    //Check for Mongoose bad ObjectId
-    if(err.name==='CastError' && err.kind === 'ObjectId'){
-        message = `Resource not found`;
-        statusCode = 404;
-    }
-
+  
+    // NOTE: checking for invalid ObjectId moved to it's own middleware
+    // See README for further info.
+  
     res.status(statusCode).json({
-        message,
-        stack: process.env.NODE_ENV === 'production'? '🍔':err.stack,
+      message: message,
+      stack: process.env.NODE_ENV === 'production' ? null : err.stack,
     });
-};
-
-export {notFound, errorHandler};
+  };
+  
+  export { notFound, errorHandler };  
