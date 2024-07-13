4ad99ec5d4f613ce8d1c4e49c43f50bb68d82e70
gupta-rajan <rjngupta22@gmail.com>
2024-07-13 23:32:04 +0530

    update mt styling

--- frontend/src/screens/MachineTranslation.jsx
+++ frontend/src/screens/MachineTranslation.jsx
@@ -17,47 +17,63 @@ const MachineTranslationScreen = () => {
         {
           source_language: sourceLanguage,
           target_language: targetLanguage,
-          source_text: inputText // Pass source_text instead of text
+          source_text: inputText
         }
       );
-      setTranslatedText(response.data.translated_text); // Set translated_text from response
+      setTranslatedText(response.data.translated_text);
     } catch (error) {
       console.error('Error translating text:', error);
-      // Handle error (e.g., show error message)
     } finally {
       setLoading(false);
     }
   };
 
+  const handleClearText = () => {
+    setInputText('');
+    setTranslatedText('');
+  };
+
   return (
     <div className="container mt-5">
-      <h1 className="text-center mb-4" style={{ padding: '10px' }}>Machine Translation</h1>
-      <div className="row justify-content-center">
+      <h1 className="text-center mb-4" style={{ padding: '10px' ,color: "#007bff"}}>Machine Translation</h1>
+      <div className="row justify-content-center rounded-lg shadow-lg p-4 bg-white">
         <div className="col-md-6">
+        <h3 className="text-center text-3xl font-bold text-blue-700 mb-6">Let's translate from text to text</h3>
           <div className="form-group">
-            <label htmlFor="sourceLanguage">Source Language:</label>
-            <select
-              id="sourceLanguage"
-              className="form-control"
-              value={sourceLanguage}
-              onChange={(e) => setSourceLanguage(e.target.value)}
-            >
-              <option value="english">English</option>
-            </select>
+            <label htmlFor="sourceLanguage" className="mt-2 mb-1">Source Language:</label>
+            <div className="input-group">
+              <select
+                id="sourceLanguage"
+                className="form-control"
+                value={sourceLanguage}
+                onChange={(e) => setSourceLanguage(e.target.value)}
+              >
+                <option value="english">English</option>
+                
+              </select>
+              {/* <div className="input-group-append"> */}
+              <span className="input-group-text"><i className="fa fa-chevron-down"></i></span>
+              {/* </div> */}
+            </div>
           </div>
           <div className="form-group">
-            <label htmlFor="targetLanguage">Target Language:</label>
-            <select
-              id="targetLanguage"
-              className="form-control"
-              value={targetLanguage}
-              onChange={(e) => setTargetLanguage(e.target.value)}
-            >
-              <option value="hindi">Hindi</option>
-            </select>
+            <label htmlFor="targetLanguage" className="mt-2 mb-1">Target Language:</label>
+            <div className="input-group">
+              <select
+                id="targetLanguage"
+                className="form-control"
+                value={targetLanguage}
+                onChange={(e) => setTargetLanguage(e.target.value)}
+              >
+                <option value="hindi">Hindi</option>
+              </select>
+              {/* <div className="input-group-append"> */}
+                <span className="input-group-text"><i className="fa fa-chevron-down"></i></span>
+              {/* </div> */}
+            </div>
           </div>
           <div className="form-group">
-            <label htmlFor="inputText">Input Text:</label>
+            <label htmlFor="inputText" className="mt-2 mb-1">Input Text:</label>
             <textarea
               id="inputText"
               className="form-control"
@@ -67,14 +83,24 @@ const MachineTranslationScreen = () => {
               style={{ marginBottom: '10px' }}
             ></textarea>
           </div>
-          <button
-            className="btn btn-primary"
-            onClick={handleTranslation}
-            disabled={loading || !inputText}
-            style={{ backgroundColor: '#007bff', borderColor: '#007bff', padding: '8px 16px', marginBottom: '10px' }}
-          >
-            {loading ? 'Translating...' : 'Translate It'}
-          </button>
+          <div className="text-center m-1">
+            <button
+              className="btn btn-primary m-2"
+              onClick={handleTranslation}
+              disabled={loading || !inputText}
+              style={{ backgroundColor: '#007bff', borderColor: '#007bff', padding: '8px 16px' }}
+            >
+              {loading ? 'Translating...' : 'Translate It'}
+            </button>
+            <button
+              className="btn btn-primary m-2"
+              onClick={handleClearText}
+              disabled={loading || !inputText}
+              style={{ backgroundColor: '#007bff', borderColor: '#007bff', padding: '8px 16px'  }}
+            >
+              Clear Text
+            </button>
+          </div>
           <div className="mt-3" style={{ border: '1px solid #007bff', borderRadius: '5px', padding: '10px' }}>
             {translatedText && (
               <div>
